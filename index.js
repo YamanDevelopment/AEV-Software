2b20fa0bbee008a406086231c6d54baf188bfc0a
Jossaya Camille <jcamille2023@fau.edu>
2024-04-13 10:46:31 -0700

    webserver

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-import { SerialPort } from 'serialport'
+import { SerialPort } from 'serialport';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+import {dirname, join} from 'node:path';
+import express from "express";
 
-// Create a port
-const port = new SerialPort({
-  path: 'COM3',
-  baudRate: 115200,
-})
 
-port.write('help', function(err) {
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+io.on("connection", (socket) => {
+  console.log(socket);
+
+  const port = new SerialPort({
+    path: 'COM3',
+    baudRate: 115200,
+  });
+  console.log(port);
+  port.write('help', function(err) {
     if (err) {
       return console.log('Error on write: ', err.message)
     }
     console.log('message written')
-  })
-
+  });
   port.on('data', function (data) {
-    console.log('Data:', data)
-  })
+    console.log('Data:', data);
+    socket.emit('data', data);
+    
+  });
+
+
   
+})
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+app.use('/',express.static(join(_dirname,"static")));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(_dirname,'/static/index.html'));
+});
+httpServer.listen(3000);
+
+
+/*
+// Create a port
+
+  */
---
+++ static/index.js
@@ -0,0 +1,13 @@
+import {io} from "/socket.io/socket.io.esm.min.js";
+
+
+const socket = io();
+
+socket.on("connect", () => {
+    console.log(socket.id);
+})
+
+socket.on('data', (data) => {
+    let container = document.getElementById("container");
+    container.innerHTML = data;
+})
