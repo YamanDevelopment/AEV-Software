a1143f2085629189746f23e257dfed8e4e26df4d
jeebuscrossaint <thediamond270@gmail.com>
2024-04-21 09:21:14 -0400

    car build proc
    Former-commit-id: 6289b2b78d6bfbd9f8ba14f1fa102bf5a8421734

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-
+  root: './static',
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors
