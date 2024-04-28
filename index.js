bb288f56752b67d0820ceafe03c633ef9eb69db0
Jossaya Camille <jcamille2023@fau.edu>
2024-04-27 20:43:13 -0400

    attempt at fixing my broken ass code

--- index.js
+++ index.js
@@ -24,33 +24,11 @@ const io = new Server(httpServer);
   });
 }
  function readData(port,bytecount) {
-  const byteparser = port.pipe(new ByteLengthParser({length: bytecount}));
-  let data;
-  byteparser.on('data', (stream) => {
-    data = stream.toString();
-    console.log(data);
-    byteparser.off('data', () => {});
-  });
-  return data;
+  
 }
 
 function getBatteryData(port) { 
-  writeData('sh\n',port); //need to later add the command to switch to battery data
-  let data = readData(port, 250);
-  let battery_data = {};
-  let data_arr = data.split('\n');
-  let new_arr = []
-  for(let item of data_arr) {
-    item = item.trim();
-    item = item.split(' ');
-    item = item.filter((el) => el != '');
-    new_arr.push(item);
-    if(item.includes(':') && !(item.includes('uptime')) && !(item.includes('alerts'))){
-      battery_data[item[0]] = item[2];
-    }
-    //if(item.includes('alerts')) {}
-    
-  }
+ 
   return battery_data;
 }
 
@@ -62,9 +40,33 @@ io.on("connection", (socket) => {
       baudRate: 115200,
     });
     let interval = setInterval(() => {
-      let data = getBatteryData(port)
+      const byteparser = port.pipe(new ByteLengthParser({length: bytecount}));
+      byteparser.on('data', (stream) => {
+        let data = stream.toString();
+        console.log(data);
+        let battery_data = {};
+        let data_arr = data.split('\n');
+        let new_arr = []
+        for(let item of data_arr) {
+          item = item.trim();
+          item = item.split(' ');
+          item = item.filter((el) => el != '');
+          new_arr.push(item);
+          if(item.includes(':') && !(item.includes('uptime')) && !(item.includes('alerts'))){
+            battery_data[item[0]] = item[2];
+          }
+          //if(item.includes('alerts')) {}
           
-      socket.emit('data', data.current);
+        }
+        socket.emit('data', battery_data);
+      });
+      writeData('sh\n',port); //need to later add the command to switch to battery data
+      
+      let data;
+     
+     
+          
+      
     }, 1000)
     // When a client connects, send battery data (github copilot wrote that)
   //
