0d906aa2d5a981a4ec878a59f22f1bd520610414
jeebuscrossaint <thediamond270@gmail.com>
2024-05-17 16:31:58 -0400

    move glb edit index
    Former-commit-id: 59763e9fdbc21091446f1a500ae5295dffd505ab

--- static/bms/index.js
+++ static/bms/index.js
@@ -1,13 +1,6 @@
 import { ipcRenderer } from "electron";
 
-
-const socket = io();
-
-socket.on("connect", () => {
-    console.log(socket.id);
-})
-
-socket.on('data', (data) => {
-    let container = document.getElementById("container");
-    console.log(data)
+ipcRenderer.send('connection');
+ipcRenderer.on('data', (data) => {
+    document.getElementById('container').innerHTML = JSON.stringify(data);
 })
