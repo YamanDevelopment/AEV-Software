a77143c0819b64eb46251cf1f4084f8502e20a2d
Thandi Menelas <TMenelas@outlook.com>
2024-05-17 16:41:41 -0400

    kms --jossaya
    Former-commit-id: 6a673d22e74c817154b3aa83344a3e27123cdded

--- static/bms/index.js
+++ static/bms/index.js
@@ -1,13 +1,2 @@
 import { ipcRenderer } from "electron";
-
-
-const socket = io();
-
-socket.on("connect", () => {
-    console.log(socket.id);
-})
-
-socket.on('data', (data) => {
-    let container = document.getElementById("container");
-    console.log(data)
-})
+let container = document.getElementById("container");
