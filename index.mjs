fd7ab09e4a5c9b188547570b00e98bdd3ccbd7ac
Jossaya Camille <jcamille2023@fau.edu>
2024-05-19 03:20:03 -0400

    New app nearly done, just maybe needs a few tweaks
    Former-commit-id: a2930c7da0002b7d1f7dfb2c44bedbc0f5fa794b

--- index.mjs
+++ index.mjs
@@ -16,6 +16,10 @@ const server = express();
 const httpServer = createServer(server);
 const io = new Server(httpServer);
 function writeData(data, port) {
+    console.log(port);
+    if(!port.isOpen) {
+        io.emit('error', 'Port is not open');
+    }
     port.write(data, function(err) {
         if (err) {
             return console.log('Error on write: ', err.message)
@@ -27,38 +31,46 @@ const port = new SerialPort({
     path: '/dev/ttyUSB0',
     baudRate: 115200,
 });
+const byteparser = port.pipe(new ByteLengthParser({
+    length: 250
+}));
+byteparser.on('data', (stream) => {
+    let data = stream.toString();
+    console.log(data);
+    let battery_data = {};
+    let data_arr = data.split('\n');
+    let new_arr = []
+    for (let item of data_arr) {
+        item = item.trim();
+        item = item.split(' ');
+        item = item.filter((el) => el != '');
+        new_arr.push(item);
+        if (item.includes(':') && !(item.includes('uptime')) && !(item.includes('alerts'))) {
+            battery_data[item[0]] = item[2];
+        }
+        //if(item.includes('alerts')) {}
+    }
+    io.emit('data', battery_data);
+});
 port.on('error', (err) => {
     io.emit('error', err);
-    const byteparser = port.pipe(new ByteLengthParser({
-        length: 250
-    }));
-    byteparser.on('data', (stream) => {
-        let data = stream.toString();
-        console.log(data);
-        let battery_data = {};
-        let data_arr = data.split('\n');
-        let new_arr = []
-        for (let item of data_arr) {
-            item = item.trim();
-            item = item.split(' ');
-            item = item.filter((el) => el != '');
-            new_arr.push(item);
-            if (item.includes(':') && !(item.includes('uptime')) && !(item.includes('alerts'))) {
-                battery_data[item[0]] = item[2];
-            }
-            //if(item.includes('alerts')) {}
-        }
-        io.emit('data', battery_data);
-    });
 });
 io.on("connection", (event) => {
-    try {
-        let interval = setInterval(() => {
-            writeData('sh\n', port);
-        }, 1000);
-    } catch (err) {
-        console.log(err);
-        io.emit('error', err);
+    if(port.isOpen) {
+        try {
+            let interval = setInterval(() => {
+                writeData('sh\n', port);
+            }, 1000);
+        } catch (err) {
+            console.log(err);
+            io.emit('error', err);
+        }
+        event.on('disconnect', () => {
+            clearInterval(interval);
+        });
+    }
+    else {
+        io.emit('error', 'Port is not open');
     }
 });
 // Static files (html, css, js) (also wrote by copilot)
