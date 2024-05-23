8076a8d630f1f24acb0a6303e248f5e33cf01598
jeebuscrossaint <thediamond270@gmail.com>
2024-05-23 10:38:05 -0400

    qt
    Former-commit-id: bdc8f738024f5b722219547ae2555aa490465b78

--- qtcpp-app-test/main.cpp
+++ qtcpp-app-test/main.cpp
@@ -97,7 +97,7 @@ private slots:
         // Camera
         Qt3DRender::QCamera *cameraEntity = view.camera();
         cameraEntity->lens()->setPerspectiveProjection(45.0f, 16.0f/9.0f, 0.1f, 1000.0f);
-        cameraEntity->setPosition(QVector3D(0, 0, 0f));
+        cameraEntity->setPosition(QVector3D(0, 0, 0));
         cameraEntity->setUpVector(QVector3D(0, 1, 0));
         cameraEntity->setViewCenter(QVector3D(0, 0, 0));
 
