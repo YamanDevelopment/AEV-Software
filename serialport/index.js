472e8871a7db7fbf1c53fcb15f0ce14114c6c1be
Jossaya Camille <jcamille2023@fau.edu>
2024-05-25 09:11:59 -0700

    fixed my regex
    Former-commit-id: 13c095688d222b67b354f0a22a6e0cd2773c31f3

--- serialport/index.js
+++ serialport/index.js
@@ -28,8 +28,8 @@ class BmsData {
         console.log(data);
         const voltagere = new RegExp("\\s+voltage\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
         const cellsre = new RegExp("\\s+cells\\s+:\\s[0-9]{2}.+");
-        const meanre = new RegExp("\\s+mean\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
-        const stddevre = new RegExp("\\s+std dev\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
+        const meanre = new RegExp("\\s+mean\\s+:\\s[0-9]+\\.[0-9]+v");
+        const stddevre = new RegExp("\\s+std dev\\s+:\\s[0-9]+\\.[0-9]+v");
         const currentre = new RegExp("\\s+current\\s+:\\s.[0-9]+\\.[0-9]+A");
         const socre = new RegExp("\\s+soc\\s+:\\s[0-9].*[0-9]*%");
         for(let item of data) {
