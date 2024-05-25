4e9ce6491a5933bce5f8f310ac273b652167bed4
ZachLTech <zlopezzl2021@gmail.com>
2024-05-25 12:08:10 -0400

    fixed stuff
    Former-commit-id: 0c4a49d59505ea063104d3e474a3e6170f738052

--- frontend/pages/bms.vue
+++ frontend/pages/bms.vue
@@ -1,20 +1,18 @@
-<script>
+<script setup>
     import {io} from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
     
-    let data = ref({
-        "voltage": "91.92v",
-        "cells": "30",
-        "mean": "3.064v",
-        "std dev": "0.126v",
-        "alerts": "pack in LVC",
-        "current": "-0.2A",
-        "SOC": "4%",
-        "uptime": "0 hour(s), 2 minute(s), 15 second(s)"
-    });
-    let error = ref({});
+    // let data = ref({
+    //     "voltage": "91.92v",
+    //     "cells": "30",
+    //     "mean": "3.064v",
+    //     "std dev": "0.126v",
+    //     "alerts": "pack in LVC",
+    //     "current": "-0.2A",
+    //     "SOC": "4%",
+    //     "uptime": "0 hour(s), 2 minute(s), 15 second(s)"
+    // });
+    // let error = ref({});
     const socket = io('http://localhost:3000', {  reconnectionDelayMax: 10000,});
-
-    /*
     socket.on('data', (content) => {
         data.value = content;
     });
@@ -22,7 +20,6 @@
         console.error("SOCKET ERROR: " + err);
         error = content;
     });
-    */
     
     // export default {
     //     data() {
@@ -41,5 +38,4 @@
     <div>
         Data: {{ JSON.stringify(data) }}
     </div>
-    
 </template>
--- serialport/index.js
+++ serialport/index.js
@@ -1,31 +1,39 @@
 import { SerialPort } from 'serialport';
-import { ByteLengthParser } from '@serialport/parser-byte-length';
+import { DelimiterParser } from '@serialport/parser-delimiter';
 import { Server } from 'socket.io';
 import {createServer} from 'http';
 import express from 'express';
+import cors from 'cors';
+
+
 
 const port = new SerialPort({
     path: '/dev/ttyUSB0',
     baudRate: 115200,
 });
-const byteparser = port.pipe(new ByteLengthParser({
-    length: 500
+const byteparser = port.pipe(new DelimiterParser({
+    delimiter: 'sh'
 }));
 port.open();
 const server = express(); // socketio server used as fallback in case of electron failure
+server.use(cors())
 const httpServer = createServer(server);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {cors: {
+    origin: '*',
+  }});
 const connections = [];
 
 class BmsData {
     constructor(data) {
+        console.log(data);
         const voltagere = new RegExp("\\s+voltage\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
         const cellsre = new RegExp("\\s+cells\\s+:\\s[0-9]{2}.+");
         const meanre = new RegExp("\\s+mean\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
-        const stddevre = new RegExp("\\s+stddev\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
+        const stddevre = new RegExp("\\s+std dev\\s+:\\s[0-9]{2}\\.[0-9]{2}v");
         const currentre = new RegExp("\\s+current\\s+:\\s.[0-9]+\\.[0-9]+A");
         const socre = new RegExp("\\s+soc\\s+:\\s[0-9].*[0-9]*%");
-        for(let item in data) {
+        for(let item of data) {
+            console.log(item);
             if(voltagere.test(item)) {
                 this.voltage = item.match(/[0-9]{2}\.[0-9]{2}/)[0];
             }
@@ -50,9 +58,8 @@ class BmsData {
 
 byteparser.on('data', (stream) => { //reads data
     let data = stream.toString().split('\n');
-    for (let item of data) {
-        let battery_data = new BmsData(item);
-    }
+    let battery_data = new BmsData(data);
+    console.log(battery_data);
     io.emit('data', battery_data);
 });
 port.on('error', (err) => {io.emit('error', err)});
