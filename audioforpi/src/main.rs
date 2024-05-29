1d15810ab06313d9f2d2d8c612ee1a234430201f
jeebuscrossaint <thediamond270@gmail.com>
2024-05-29 12:38:18 -0400

    Builds in Unix

--- audioforpi/src/main.rs
+++ audioforpi/src/main.rs
@@ -1,3 +1,14 @@
+use rppal::gpio::Gpio;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut pin = Gpio::new().unwrap().get(17).unwrap().into_output();
+
+    for _ in 0..5 {
+        pin.set_high();
+        thread::sleep(Duration::from_secs(1));
+        pin.set_low();
+        thread::sleep(Duration::from_secs(1));
+    }
 }
