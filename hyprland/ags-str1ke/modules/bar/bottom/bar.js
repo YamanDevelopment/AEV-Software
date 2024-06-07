0d0b0006afb2edec4a6954ed56f52946df05223c
Thandi M. <TMenelas@outlook.com>
2024-06-07 12:28:36 -0400

    ags
    Former-commit-id: e85e204ebc4e83b1542056872296d89340dab68b

---
+++ hyprland/ags-str1ke/modules/bar/bottom/bar.js
@@ -0,0 +1,59 @@
+import { Widget } from '../../utils/imports.js';
+const { Box, CenterBox, Window } = Widget;
+import BM from './modules.js';
+
+function Left() {
+    const left = Box({
+        children: [
+            // BM.Launcher(),
+            // BM.Separator("|"),
+            BM.Workspaces(),
+            BM.ClientTitle(),
+        ],
+    });
+    return left;
+}
+
+// function Center() {
+//     const center = Box({
+//         children: [
+//             BM.Notification(),
+//             BM.Separator(),
+//             BM.Clock('button'),
+//             BM.Separator(),
+//             BM.Media(),
+//         ],
+//     });
+//     return center;
+// }
+
+function Right() {
+    const right = Box({
+        hpack: 'end',
+        children: [
+            BM.VolumeInfo(),
+            BM.NetworkInfo(),
+            // BM.BatteryInfo(),
+            BM.Separator(),
+            // BM.SystemInfo(),
+            // BM.Separator(),
+            BM.SysTray(),
+            BM.Separator(),
+            BM.PowerMenu(),
+        ],
+    });
+    return right;
+}
+
+export default monitor => Window({
+    name: `bbar${monitor}`, // name has to be unique
+    monitor,
+    anchor: ['bottom', 'left', 'right'],
+    exclusivity: 'exclusive',
+    child: CenterBox({
+        className: 'bar',
+        startWidget: Left(),
+        // centerWidget: Center(),
+        endWidget: Right(),
+    }),
+})
