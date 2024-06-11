d6a66d1c0c9e97272ae971f70998c6f5f98b81aa
Thandi M. <TMenelas@outlook.com>
2024-06-11 15:09:34 -0400

    Fixing up setup script and hypr conf
    Former-commit-id: 734e79f1d945b24c3802b950790ca36477d4e0cd

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+# Tested OS: Arch Linux for ARM
+# Requirements: node, npm, git, tmux, gpsd
+
+# Clone the repository
+echo "Cloning the repository..."
+git clone https://github.com/jeebuscrossaint/aev-software ~/aev-software
+
+# Symlink Hyprland and AGS configuration
+echo "Symlinking Hyprland and AGS configuration..."
+mv ~/.config/hypr ~/.config/hypr.bak
+ln -sf ~/aev-software/hyprland/hypr ~/.config/hypr
+mv ~/.config/ags ~/.config/ags.bak
+ln -sf ~/aev-software/hyprland/ags-str1ke ~/.config/ags
+
+# Install frontend dependencies
+echo "Installing frontend dependencies..."
+cd ~/aev-software/frontend
+npm install
+
+# Install serialport dependencies
+echo "Installing serialport dependencies..."
+cd ~/aev-software/serialport
+npm install
