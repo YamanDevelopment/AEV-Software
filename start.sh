a274b8008d87c307c7a8fb93a6373c13d607c5a3
ZachLTech <zlopezzl2021@gmail.com>
2024-06-12 16:59:52 -0400

    revert
    Former-commit-id: be9f383b650733f7e62f40692819084ed9302e27

--- start.sh
+++ start.sh
@@ -4,9 +4,7 @@
 # tmux split-window -h 'fish'
 # tmux split-window -h
 
-tmux new-session -s "debug" -d "bash -c 'cd frontend && npm run dev'"
+tmux new-session -d "bash -c 'cd frontend && npm run dev'"
 tmux split-window -h "bash -c 'cd serialport && sudo npm start && cat'"
 
-ags &
-hyprpaper &
-# kitty --detach --title "debug" "tmux -2 attach-session -d"
+kitty --detach --title "debug" "tmux -2 attach-session -d"
