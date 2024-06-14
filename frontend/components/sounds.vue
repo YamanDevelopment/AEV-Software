b2fcdf1d197498d4fab7d66181036ea4c99a1887
ZachLTech <zlopezzl2021@gmail.com>
2024-06-14 17:32:15 -0400

    SOUNDS + stop button
    Former-commit-id: 04c32383e9537c1cf2c761c796f7f991d92b764a

--- frontend/components/sounds.vue
+++ frontend/components/sounds.vue
@@ -1,21 +1,32 @@
 <script setup>
+    const audioElements = ref([]);
+
     function playSound(soundNumber) {
-      let audio = new Audio(`/sound${soundNumber}.mp3`);
-      audio.play();
+        let audio = new Audio(`/sound${soundNumber}.mp3`);
+        audio.play();
+        audioElements.value.push(audio);
+    }
+
+    function stopSounds() {
+        audioElements.value.forEach(audio => {
+            audio.pause();
+            audio.currentTime = 0;
+        });
+        audioElements.value = [];
     }
 </script>
 
 <template>
     <div class="grid grid-cols-3 gap-4 py-[16px] px-[13px]">
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(1)">Dolphin (Test)</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(2)">Sound 2</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(3)">Sound 3</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(4)">Sound 4</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(5)">Sound 5</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(6)">Sound 6</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(7)">Sound 7</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(8)">Sound 8</button>
-        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover flex justify-center items-center" @click="playSound(9)"><p class="absolute text-7xl">🚨</p></button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(1)">Engine Start</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(2)">Racecar</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(3)">Reversing</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(4)">UFO</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(5)">Walking</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(6)">Ukulele</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(7)">Horn 1</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="playSound(8)">Horn 2</button>
+        <button class="bg-gray-200 text-black rounded shadow-md transition-transform hover:bouncy-hover" @click="stopSounds()">STOP NOISE</button>
     </div>
 </template>
 
