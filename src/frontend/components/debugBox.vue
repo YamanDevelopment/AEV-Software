15c4a4eab74e7b328d2e6f5e61730b49f9c6137b
ZachLTech <zlopezzl2021@gmail.com>
2024-07-07 12:55:24 -0400

    thandi/zach doing some debug page stuff

---
+++ src/frontend/components/debugBox.vue
@@ -0,0 +1,45 @@
+<script setup>
+    // let protocol 
+    // let intervalTime
+    // console.log(props.protocol)
+    const props = defineProps(['protocol', 'intervalTime'])
+    const messages = ref([props.protocol]);
+
+    const socket = new WebSocket("ws://10.8.0.5:3001");
+    socket.onopen = (event) => {
+        socket.send(props.protocol);
+    };
+
+    // Message Handler
+    socket.onmessage = (event) => {
+        // Update All data
+        console.log(event.data)
+        messages.value.push(event.data);
+    }
+    // onMounted(() => {
+    //     setTimeout(() => {
+            
+    //     }, 5000);
+    // });
+</script>
+
+<template>
+    <div id="rawDataContainer" class="w-full h-full px-12 flex flex-col gap-3 overflow-scroll border-4 border-black rounded-xl">
+        <p v-for="message in messages">
+            {{ message }}
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            protocol: {
+                type: String
+            },
+            intervalTime: {
+                type: Number
+            }
+        }
+    }
+</script>
---
+++ src/frontend/pages/debug.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+    // const socket = new WebSocket("ws://10.8.0.5:3001");
+    // // Message Handler
+    // socket.onmessage = (event) => {
+
+    // }
+    
+    function sendGoogleSheets(){
+        socket.send("lap-sheets");
+    }
+    function restartBMS(){
+        socket.send("bms-restart");
+    }
+    function restartGPS(){
+        socket.send("gps-restart");
+    }
+</script>
+
+<template>
+    <div class="flex flex-col gap-5 w-screen h-screen items-center justify-center">
+        <div class="flex gap-4 justify-center items-center w-full">
+            <button @click="sendGoogleSheets" class="text-3xl font-semibold ml-2 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 focus:outline-none">
+                Send Data to Google Sheets
+            </button>
+            <button @click="restartBMS" class="text-3xl font-semibold ml-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none">
+                Restart BMS
+            </button>
+            <button @click="restartGPS" class="text-3xl font-semibold ml-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 focus:outline-none">
+                Restart GPS
+            </button>
+        </div>
+        <div class="flex gap-4 justify-center items-center w-full h-[90%] px-16">
+            <debugBox :timeInterval="600" :protocol="'bms-alerts'" />
+            <debugBox :timeInterval="650" :protocol="'bms-debug'" />
+        </div>
+    </div>
+</template>
