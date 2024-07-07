571cc1056aeb77adc00e1d769f8df7e53f752731
Thandi M. <TMenelas@outlook.com>
2024-07-07 00:34:43 -0400

    Working on scripts
    Former-commit-id: 869ca0d4cd3b62faf94660f09cd761c779567299

--- setup.sh
+++ setup.sh
@@ -10,21 +10,26 @@
 # Symlink Hyprland and AGS configuration
 echo "Symlinking Hyprland and AGS configuration..."
 mv ~/.config/hypr ~/.config/hypr.bak
-ln -sf ~/aev-software/src/hyprland/hypr ~/.config/hypr
+ln -sf ~/aev-software/src/hyprland/config/hypr ~/.config/hypr
 mv ~/.config/ags ~/.config/ags.bak
-ln -sf ~/aev-software/src/hyprland/ags-str1ke ~/.config/ags
+ln -sf ~/aev-software/src/hyprland/config/ags ~/.config/ags
 
 # Install frontend dependencies
 echo "Installing frontend dependencies..."
 cd ~/aev-software/src/frontend
 npm install
 
-# Install serialport dependencies
-echo "Installing serialport dependencies..."
-cd ~/aev-software/src/serialport
+# Install backend dependencies
+echo "Installing backend dependencies..."
+cd ~/aev-software/src/backend
 npm install
 
 # Install system dependencies
 echo "Installing system dependencies (may require superuser priveleges)..."
-sudo pacman -S npm hyprland gpsd
+sudo pacman -S npm hyprland gpsd node git tmux kitty 
 # Should prob install AUR packages here but doesn't really matter rn will do so later - Thandi
+
+# Setup binaries
+echo "Setting up binaries..."
+ln -sf ~/aev-software/bin/start_aev_tmux /usr/bin/start_aev_tmux
+ln -sf ~/aev-software/bin/amplitude /usr/bin/amplitude
--- start.sh
+++ start.sh
@@ -1,41 +1,17 @@
-#!/bin/bash
-
-# tmux new-session -d 'bash'
-# tmux split-window -h 'fish'
-# tmux split-window -h
-
-# tmux new-session -d "bash -c 'cd src/frontend && npm run dev && cat'"
-# tmux split-window -h "bash -c 'cd ../backend && sudo npm start && cat'"
-
-# kitty --detach --title "debug" "./tmux.sh"
+#!/usr/bin/env bash
 
 SESSION_NAME="AEV-Software"
 
-# command_exists(){
-#   command -v "$1" > /dev/null 2>&1
-# }
+tmux new-session -d -s $SESSION_NAME
+tmux split-window -h 
 
-# if command_exists kitty && command_exists hyprctl; then
+tmux select-pane -t 0
+tmux send-keys 'cd /root/aev-software/src/frontend' C-m 
+tmux send-keys 'npm run dev && cat' C-m 
 
-#  kitty bash -c "
+tmux select-pane -t 1 
+tmux send-keys 'cd /root/aev-software/src/backend' C-m 
+tmux send-keys 'sudo node main.js && cat' C-m 
 
-    tmux new-session -d -s $SESSION_NAME
-    tmux split-window -h 
 
-    tmux select-pane -t 0
-    tmux send-keys 'cd /root/aev-software/src/frontend' C-m 
-    tmux send-keys 'npm run dev && cat' C-m 
-
-    tmux select-pane -t 1 
-    tmux send-keys 'cd /root/aev-software/src/backend' C-m 
-    tmux send-keys 'sudo node main.js && cat' C-m 
-
-#    tmux attach -t $SESSION_NAME
-#  " &
-
-#  sleep 2
-#  kitty_pid=$(pgrep -n kitty)
-#  hyprctl dispatch movetoworkspace 5 address:$kitty_pid
-# fi
-
-kitty --detach --title "aev-debug" "./tmux.sh"
+kitty --detach --title "aev-debug" "start_aev_tmux"
